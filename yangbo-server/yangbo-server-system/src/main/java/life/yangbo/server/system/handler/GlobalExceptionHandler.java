3eaa3bf42c6d4f755a821f649de19086ebe75d2a
appleM1 <ybvip@outlook.com>
2021-04-21 16:46:03 +0800

    全局异常处理器（处理Controller层）

---
+++ yangbo-server/yangbo-server-system/src/main/java/life/yangbo/server/system/handler/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package life.yangbo.server.system.handler;
+
+import life.yangbo.common.handler.BaseExceptionHandler;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理类
+ * 全局处理Controller层抛出来的异常
+ */
+@RestControllerAdvice
+@Order(value = Ordered.HIGHEST_PRECEDENCE)
+public class GlobalExceptionHandler extends BaseExceptionHandler {
+}
